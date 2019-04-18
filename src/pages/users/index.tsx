52cb32fbe2c80bfbd8d54b94565eef3775c58fd7
Xiaochuan Ma <mxcins@gmail.com>
2019-04-18 17:42:37 +0800

    update

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -4,14 +4,16 @@ import { Spin, Table } from 'antd';
 import React, { SFC } from 'react';
 import { QueryResult } from 'react-apollo';
 
+import { GQLUser } from '@/graphql/generated';
+
 import * as services from './index.gql';
 
 const columns = [{ key: 'id', dataIndex: 'id' }, { key: 'name', dataIndex: 'name' }];
 
-const Users: SFC<IRouteComponentProps & QueryResult> = props => {
+const Users: SFC<IRouteComponentProps & QueryResult<{ users: GQLUser[] }>> = props => {
   return (
     <Spin spinning={props.loading}>
-      <Table columns={columns} dataSource={props.data.users} />
+      <Table rowKey="id" columns={columns} dataSource={props.data && props.data.users} />
     </Spin>
   );
 };
