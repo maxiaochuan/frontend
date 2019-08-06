c9cc69bfd4103b5a13da795dcc9d873c5a4a5576
Xiaochuan Ma <mxcins@gmail.com>
2019-08-06 21:12:57 +0800

    base form && join page

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,7 @@
+import React, { SFC } from 'react';
+
+const Form: SFC = () => {
+  return <div> form </div>;
+};
+
+export default Form;
---
+++ src/pages/join/index.tsx
@@ -0,0 +1,14 @@
+import { IRouteComponentProps } from '@mxcins/types';
+import React, { SFC } from 'react';
+
+import { Form } from '@/components';
+
+const Join: SFC<IRouteComponentProps> = () => {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+};
+
+export default Join;
