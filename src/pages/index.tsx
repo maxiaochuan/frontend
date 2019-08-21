83803d438d5f84ab63e52b46bdd03a337264bb01
Xiaochuan Ma <mxcins@gmail.com>
2019-08-21 15:15:56 +0800

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
-import { request } from '@/utils';
 import { IRouteComponentProps } from '@mxcins/types';
-import React, { SFC, useEffect, useState } from 'react';
+import React, { SFC } from 'react';
 import Link from 'umi/link';
 
 const Home: SFC<IRouteComponentProps> = () => {
-  const [users, setUsers] = useState<any[]>([]);
-
-  useEffect(() => {
-    request('/users.json').then(data => setUsers(data));
-  }, []);
-
   return (
     <div>
       <Link to="/chat">Chat</Link>
-      {JSON.stringify(users)}
     </div>
   );
 };
