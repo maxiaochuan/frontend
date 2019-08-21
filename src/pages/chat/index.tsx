48a9723b7f7390d213a37bea42784ae4842a0228
Xiaochuan Ma <mxcins@gmail.com>
2019-08-21 13:23:40 +0800

    send message

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,21 +1,67 @@
-import React, { SFC, useEffect } from 'react';
+import React, { Component } from 'react';
 import io from 'socket.io-client';
 import router from 'umi/router';
+import { Button, Input } from '@/components';
 
-const Chat: SFC = () => {
-  useEffect(() => {
+export default class Chat extends Component<
+  any,
+  { connected: boolean; messages: string[]; input: string }
+> {
+  public socket: any;
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      connected: false,
+      messages: [],
+      input: '',
+    };
+  }
+
+  public componentDidMount() {
     const socket = io.connect('/socket/chat');
-    socket.on('connect', () => {
-      socket.emit('chat message', 'abc');
-    });
+    socket.on('connect', () => this.setState({ connected: true }));
     socket.on('error', (error: string) => {
       if (error === 'Authorization Error') {
         router.push('/login');
       }
     });
-  }, []);
+    socket.on('chat message', (msg: string) =>
+      this.setState({ messages: [...this.state.messages, msg] }),
+    );
+    this.socket = socket;
+  }
 
-  return <div>chart</div>;
-};
+  public onChange = (e: any) => this.setState({ input: e.target.value });
 
-export default Chat;
+  public send = () => {
+    if (this.socket && this.state.connected) {
+      this.socket.emit('chat message', this.state.input);
+      this.setState({ input: '' });
+    }
+  };
+  public render() {
+    const { connected, messages, input } = this.state;
+    return (
+      <div>
+        <h2>Chat</h2>
+        <div>
+          <span>status: </span>
+          <span>{connected ? 'connected' : 'false'}</span>
+        </div>
+        <div>
+          <ul>
+            {messages.map((m, i) => (
+              <li key={`${i}`}>{m}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <Input value={input} onChange={this.onChange} />
+        </div>
+        <div>
+          <Button onClick={this.send}>Send</Button>
+        </div>
+      </div>
+    );
+  }
+}
